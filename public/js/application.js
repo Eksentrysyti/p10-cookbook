a0ece11ad845386f38a70102bfed963a2824fcbb
Steven Leu <steven.c.leu@gmail.com>
2014-07-29 00:12:14 -0700

    Adds Javascript functionality to prepopulate ingredients container with user's saved ingredients

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {  
 	$('.add-ingredient').on('click', addIngredient);
+	$('.find-recipes').on('click', findRecipes);
+	populateIngredients();
 });
+
+function populateIngredients(){
+	var userId = parseInt($('div.user-id').attr("value"));
+
+	var request = $.ajax({
+		url: '/ingredients',
+		type: 'GET',
+		data: { user_id: userId }
+	})
+	request.done(populateIngredientsDOM);
+}
+
+function populateIngredientsDOM(data){
+	ingredient_list = JSON.parse(data)
+	$.each(ingredient_list, createbuildIngredient)
+}
+
+function createbuildIngredient(index, value) {
+	var ingredientItem = buildIngredient(value.name, value.id)
+	$('.ingredient-list').append(ingredientItem)
+}
 
 function addIngredient(event){
 	event.preventDefault();
@@ -25,6 +48,12 @@ function addIngredientDOM(data){
 	var ingredientId = JSON.parse(data).ingredient_id;
 	var ingredientName = JSON.parse(data).ingredient_name;
 	var ingredientItem = buildIngredient(ingredientName, ingredientId);
-	
+
 	$('.ingredient-list').append(ingredientItem);
+}
+
+function findRecipes(event){
+	event.preventDefault();
+
+	
 }
