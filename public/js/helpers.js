37160f41ee44a62858ab15c73f543a76315746da
Steven Leu <steven.c.leu@gmail.com>
2014-07-28 22:38:27 -0700

    Creates helpers.js file Moves task of creating a new ingredient item for ingredient-container to a helper method buildIngredient

---
+++ public/js/helpers.js
@@ -0,0 +1,11 @@
+function buildIngredient(ingredientName, ingredientId) {
+  // gets Ingredient Template stored in DOM
+  // Finds the ingredient_template that is hidden and gets the template contents
+  var ingredientTemplate = $.trim($('.ingredient-template').html());
+  // Creates an jQueryDOMElement from the ingredientTemplate
+  var $ingredient = $(ingredientTemplate);
+  // Modifies its text to use the passed-in ingredientName
+  $ingredient.text(ingredientName).attr("id", ingredientId);
+  // Returns the jQueryDOMElement to be used elsewhere
+  return $ingredient
+}
